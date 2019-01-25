972119f1b26c691d70b65f6f4d1ab773caea1f6e
Wenqi <vickey9.lin@gmail.com>
2019-01-25 14:46:28 +0100

    add: some example data

--- src/App.js
+++ src/App.js
@@ -1,17 +1,25 @@
 import React from 'react'; 
 import './App.css';
 import TodoItem from "./TodoItem"
+import todoData from "./todoData"
 
-function App(){
-  return(
-    <div className="todo-list">
-      <TodoItem />
-      <TodoItem />
-      <TodoItem />
-      <TodoItem />
-      <TodoItem />
-    </div>
-  )
+class App extends React.Component {
+  constructor() {
+      super()
+      this.state = {
+          todos: todoData
+      }
+  }
+  
+  render() {
+      const todoItems = this.state.todos.map(item => <TodoItem key={item.id} item={item}/>)
+      
+      return (
+          <div className="todo-list">
+              {todoItems}
+          </div>
+      )    
+  }
 }
 
 export default App;
--- src/TodoItem.js
+++ src/TodoItem.js
@@ -1,10 +1,14 @@
 import React from "react"
 
-function TodoItem() {
+function TodoItem(props) {
     return (
         <div className="todo-item">
-            <input type="checkbox" />
-            <p>Placeholder text here</p>
+            <input 
+                type="checkbox" 
+                checked={props.item.completed} 
+                onChange={() => console.log("Changed!")}
+            />
+            <p>{props.item.text}</p>
         </div>
     )
 }
